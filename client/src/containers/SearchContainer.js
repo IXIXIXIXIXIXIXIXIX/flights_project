8efc64efbbf52dc9495e7b69260c998a8071ce51
IXIXIXIXIXIXIXIXIX <ix.scott.anderson@gmail.com>
2021-02-26 20:48:15 +0000

    implemented 3words converters

--- client/src/containers/SearchContainer.js
+++ client/src/containers/SearchContainer.js
@@ -1,33 +1,72 @@
 import React, {useState, useEffect} from 'react';
-import threeWordsToCoords from '../helpers/ThreeWordsToCoords';
+import apiKeys from '../assets/ApiKeys';
+const api = require("@what3words/api");
+
+
+
+            
+const key = apiKeys.threeWords.key;
+api.setOptions({ key: key });
+// import threeWordsToCoords from '../helpers/ThreeWordsToCoords';
+
 
 
 const SearchContainer = () => {
 
-    const testCoords = threeWordsToCoords("filled", "count", "spreading");
 
     const [flightsFound, setFlightsFound] = useState(null);
     const [selectedFlight, setFlight] = useState(null);
+    const [searchCoords, setSearchCoords] = useState(null);
+    const [locationWords, setLocationWords] = useState(null);
+
+
+    // Currently useEffect is only used to stop infinite loops when testing 3w to coords function
+    // useEffect(() => {
+        //CodeClan coordinates
+        // threeWordsToCoords("serve.sweep.kicked");
+    // }, []);
+
+///////////////////////////////////////////////////////////////////////////
+
+    function threeWordsToCoords(searchString) {
+        console.log("key used:", key);
+
+        api.convertToCoordinates(searchString)
+        .then(data => setSearchCoords(data));
+    };
+
+
+    function coordsToThreeWords(coordObj) {
+        console.log("key used:", key);
+
+        api.convertTo3wa(coordObj)
+        .then(data => setLocationWords(data));
+    };
+////////////////////////////////////////////////////////////////////////////
+
 
     // handleSearch()
-
-
-    if (testCoords.lng){
+    
+    if (searchCoords)
+    {
         return (
 
+            // Render List here
             <div>
-            <p>Latitude: {testCoords.lat}</p>
-            <p>Longitude: {testCoords.lng}</p>
-            <p>Nearest: {testCoords.nearestPlace}</p>
+            <p>Latitude: {searchCoords.coordinates.lat}</p>
+            <p>Longitude: {searchCoords.coordinates.lng}</p>
+            <p>Nearest: {searchCoords.nearestPlace}</p>
+
             </div>
-        );
+        )
     }
     else
     {
-        return(
-        <p>loading coordinate data...</p>
-        );
+        // Render searchbox here
+        return (<h1>Search Now!</h1>);
     }
+    
+
 }
 
 export default SearchContainer;
