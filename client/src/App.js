87b837ed45c93901e50c3e177c65cae09d36ff5f
IXIXIXIXIXIXIXIXIX <ix.scott.anderson@gmail.com>
2021-02-26 22:28:28 +0000

    add static external BG image

--- client/src/App.js
+++ client/src/App.js
@@ -3,30 +3,43 @@ import React, {useState, useEffect} from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import SearchContainer from './containers/SearchContainer';
 
+import BgImg from './bluesky_plane.jpg';
 
 
 function App() {
 
-  const bgPath = "./assets/jelleke-vanooteghem-gnxb59lGU1M-unsplash.jpg"
-  return ( 
-    <div className="App" style={{ backgoundImage: `url(${bgPath})`}}>
-      
-        <header className="header">
-              {/* <NavBar handleBGChange={handleBGChange} downloadLink={currentBackGround["url"]}/> */}
-        </header>
-            
-            <main className="main-body">
-      <Router>
-        <>
-          <Switch>
-            <Route path="/search" component={SearchContainer} />
-            {/* <Route component={NotFound} /> */}
-            </Switch>
-        </>
-      </Router>
-            </main>
-    </div>
-  );
+  
+  // Free stock photo for inital bckground from https://www.pexels.com/photo/white-plane-on-blue-sky-164646/
+  const BgImg = "/bluesky_plane.jpg";
+
+  const [bgImage, setBgImage] = useState(null);
+
+  useEffect(() => {
+    setBgImage(BgImg);
+  }, []);
+
+
+    return ( 
+      <div className="App" style={{ backgroundImage: `url("https://images.pexels.com/photos/164646/pexels-photo-164646.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")` }}>
+
+        {/* <img src={bgImage}/> */}
+        
+          <header className="header">
+                {/* <NavBar handleBGChange={handleBGChange} downloadLink={currentBackGround["url"]}/> */}
+          </header>
+              
+              <main className="main-body">
+        <Router>
+          <>
+            <Switch>
+              <Route path="/search" component={SearchContainer} />
+              <Route component={SearchContainer} />
+              </Switch>
+          </>
+        </Router>
+              </main>
+      </div>
+    );
 }
 
 export default App;
