3a58c209e00a58f232336a5344c4b0906bcbd5c9
IXIXIXIXIXIXIXIXIX <ix.scott.anderson@gmail.com>
2021-03-01 10:57:42 +0000

    Split results into components

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,20 +1,12 @@
 import React from 'react';
 
-const Results = () => {
+const Results = ({selectedFlight, flightFurtherInfo}) => {
 
     return (
 
         <div className="main-results-row">
 
-            <div className="result-box destination-box transparent-box in-from-right">
-                <h4>Destination</h4>
-            </div>
-            <div className="result-box position-box transparent-box in-from-bottom">
-                <h4>Position</h4>
-            </div>
-            <div className="result-box origin-box transparent-box in-from-left">
-                <h4>Origin</h4>
-            </div>
+
         </div>
     );
 };
