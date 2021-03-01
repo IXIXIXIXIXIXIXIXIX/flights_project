3ba078d6691dfd32ca5a53e8a6e135e2c0184704
Tom Pies <tombo@Toms-MacBook-Pro.local>
2021-03-01 16:27:53 +0000

    added airport data to mongodb

--- server/server.js
+++ server/server.js
@@ -1,26 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const parser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const createRouter = require('./helpers/create_router.js');
-const cors = require('cors');
+const MongoClient = require("mongodb").MongoClient;
+const createRouter = require("./helpers/create_router.js");
+const cors = require("cors");
 
 app.use(cors());
 app.use(parser.json());
 
+MongoClient.connect("mongodb://localhost:27017")
+  .then((client) => {
+    const db = client.db("airportdb");
+    const airport_dataCollection = db.collection("airport_data");
+    const airport_dataRouter = createRouter(airport_dataCollection);
+    app.use("/api/airport_data", airport_dataRouter);
+  })
+  .catch(console.err);
 
-MongoClient.connect('mongodb://localhost:27017', (err, client) => {
-    if (err)
-    {
-        console.log(err);
-    }
-    const db = client.db('skyrabble');
-    const bookingsCollection = db.collection('countries');
-    const bookingsRouter = createRouter(bookingsCollection);
-    app.use('/api/countries', bookingsRouter);
-
-    app.listen(5001, function() {
-        console.log(`App listening on port ${this.address().port}`);
-    })
-
+app.listen(5000, function () {
+  console.log(`Listening on port ${this.address().port}`);
 });
