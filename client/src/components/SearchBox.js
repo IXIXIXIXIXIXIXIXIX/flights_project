4354c623c7e3e663a8af1997446f8cdc8cd946e3
Tom Pies <tombo@Toms-MacBook-Pro.local>
2021-03-02 16:38:01 +0000

    logo added to splash

--- client/src/components/SearchBox.js
+++ client/src/components/SearchBox.js
@@ -19,6 +19,9 @@ const SearchBox = ({searchFlight}) => {
 
   return (
     <>
+            <div className="main-logo">
+              <img src="http://localhost:3000/images/skyrabble_main.png"></img>
+            </div>
     <div className="instructions transparent-box in-from-left">
       <h2>How to search</h2>
       <ol>
