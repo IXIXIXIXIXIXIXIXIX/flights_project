5c5366842244b95d5ea11b38af274a11c95d209d
IXIXIXIXIXIXIXIXIX <ix.scott.anderson@gmail.com>
2021-03-02 21:11:59 +0000

    add callback to reset to navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const NavBar = () => {
+const NavBar = ({handleNextClick}) => {
 
     function refresh(){
         window.location.reload("Refresh")
@@ -12,7 +12,7 @@ const NavBar = () => {
                      <i className="fas fa-arrow-down"></i>
 
                  <nav className="navbar-right-cluster">
-                     <span className="navbar-link tooltip"><i className="fas fa-search"></i>
+                     <span className="navbar-link tooltip" onClick={handleNextClick}><i className="fas fa-search"></i>
                             <span className="tooltiptext-left transparent-box">New Search</span>
                      </span>
                      <span className="navbar-link tooltip" onClick={() => {refresh()}}><i className="fas fa-sync"></i>
