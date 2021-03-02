c8d15f67c919084d835ad47a18a75eb623463b9b
student <student@students-MacBook-Pro.local>
2021-03-02 20:51:10 +0000

    added result map and slight change to list details

--- client/src/components/AirportPanel.js
+++ client/src/components/AirportPanel.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const AirportPanel = ({airport, airportLabel, animationDirection}) => {
 
@@ -7,7 +8,6 @@ const AirportPanel = ({airport, airportLabel, animationDirection}) => {
     if (airport)
     {
         return (
-
                 <div className={`result-box origin-box transparent-box ${animationDirection}`}>
                 <div className="result-main-head">{airportLabel}: <span className="result-content">{airport.name}</span></div>
                 <div className="result-secondary-head">City<span className="result-content">{airport.city}</span></div>
@@ -17,6 +17,21 @@ const AirportPanel = ({airport, airportLabel, animationDirection}) => {
                 <div className="result-item">Longitude: <span className="result-content"> {airport.long}</span></div>
                 <div className="result-item">ICAO code: <span className="result-content">{airport.icao_code}</span></div>
                 <div className="result-item">IATA code: <span className="result-content">{airport.iata_code}</span></div>
+            <div className="maps_container">
+                <div className="results-map" id="map">
+                <MapContainer id="map" className="results_map" center={[airport.lat, airport.long]} zoom={10} scrollWheelZoom={false}>
+                    <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[airport.lat, airport.long]}>
+                    <Popup>
+                    {airport.name}
+                </Popup>
+                </Marker>
+                </MapContainer>
+            </div>
+                </div>
                 </div>
         );
     }
